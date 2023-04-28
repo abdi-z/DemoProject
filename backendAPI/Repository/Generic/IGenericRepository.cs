aa36949ab73c9069141d2ed99d815f8f3d1ad421
Abdul Rehman Aziz <abdulrehmanx911@gmail.com>
2023-04-28 20:16:17 +0500

    test

--- backendAPI/Repository/Generic/IGenericRepository.cs
+++ backendAPI/Repository/Generic/IGenericRepository.cs
@@ -10,6 +10,7 @@ namespace backendAPI.Repository.Generic
         Task InsertRangeAsync(List<T> entities);
         Task UpdateAsync(T entity);
         Task UpdateRangeAsync(List<T> entities);
+
         Task DeleteAsync(T entity);
         Task DeleteRangeAsync(List<T> entities);
         IQueryable<T> FindAll(Expression<Func<T, bool>> predicate);
